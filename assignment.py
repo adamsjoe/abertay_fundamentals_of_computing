e9ca3a145f418dbc68ab26c520c153903fe28ded
Joseph Adams <joseph.adams@blueyonder.co.uk>
2025-09-28 14:58:51 +0100

    chore: simplified the main application logic

--- assignment.py
+++ assignment.py
@@ -7,7 +7,6 @@ from ui import MyWindow
 FILENAME_IN = 'Road_Accident_Data.csv'
 
 def open_file(file_in, skip_header=True):
-    print(f'Opening file: {file_in}')
     data = []
     try:
         with open(file_in, encoding='utf-8-sig', mode='r') as file:
@@ -19,28 +18,18 @@ def open_file(file_in, skip_header=True):
         print(f'File {file_in} does not exist')
         sys.exit(1)
     except Exception as e:
-        print(f'Opening file {file_in} failed: {e}')
+        print(f'Failed to open file {file_in}: {e}')
         sys.exit(1)
     return data
 
-def getYears(data):
+def get_years(data):
     return sorted({row[ACCIDENT_DATE].split("-")[2] for row in data})
-
-def getPolice(data):
-    return sorted({row[POLICE_FORCE] for row in data})
-
-def getLocalAuthority(data):
-    return sorted({row[LOCAL_AUTHORITY_DISTRICT] for row in data})
 
 if __name__ == "__main__":
     data = open_file(FILENAME_IN)
-    print(f"Number of rows: {len(data)}")
-
-    years = getYears(data)
-    police = getPolice(data)
-    localAuthorities = getLocalAuthority(data)
+    years = get_years(data)
 
     app = QApplication(sys.argv)
-    window = MyWindow(years, police, localAuthorities, data)
+    window = MyWindow(years, data)
     window.showFullScreen()
     sys.exit(app.exec())
