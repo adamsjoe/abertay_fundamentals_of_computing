e57f9a5f3624c17d3df9847ba7ef1758c4ac7f24
Joseph Adams <joseph.adams@blueyonder.co.uk>
2025-09-28 13:21:55 +0100

    moved qa_test  to a new spike directory

--- spikeFiles/qt_test.py
+++ spikeFiles/qt_test.py
@@ -1,13 +1,23 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QComboBox, QPushButton
+from PyQt6.QtWidgets import (
+    QApplication, QWidget, QGridLayout, QLabel, QComboBox, QPushButton,
+    QMainWindow, QMessageBox
+)
+from PyQt6.QtGui import QKeyEvent
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction 
 
-class MyWindow(QWidget):
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Data Selector")
         self.initUI()
 
     def initUI(self):
+        # Central widget
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
         layout = QGridLayout()
 
         # Year
@@ -48,7 +58,29 @@ class MyWindow(QWidget):
         layout.addWidget(self.local_combo, 0, 5)
         layout.addWidget(self.show_button, 0, 6)
 
-        self.setLayout(layout)
+        central_widget.setLayout(layout)
+
+        # Menu Bar
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu("File")
+        help_menu = menubar.addMenu("Help")
+
+        # Exit action
+        exit_action = QAction("Exit", self)
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
+
+        # About action
+        about_action = QAction("About", self)
+        about_action.triggered.connect(self.show_about)
+        help_menu.addAction(about_action)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        """Exit full screen if Escape is pressed."""
+        if event.key() == Qt.Key.Key_Escape:
+            self.close()  
+        else:
+            super().keyPressEvent(event)
 
     def show_results(self):
         year = self.year_combo.currentText()
@@ -56,8 +88,11 @@ class MyWindow(QWidget):
         local = self.local_combo.currentText()
         print(f"Year: {year}, Police Force: {police}, Local Authority: {local}")
 
+    def show_about(self):
+        QMessageBox.information(self, "About", "Data Selector v1.0\nCreated with PyQt6")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
-    window.showFullScreen()  # <-- full screen mode
+    window.showFullScreen()
     sys.exit(app.exec())
